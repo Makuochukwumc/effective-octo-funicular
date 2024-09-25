91961aaa8d9dda7656d79c631d8c760f4c4941b1
Makuochukwumc <makuochukwucollins44@gmail.com>
2024-09-25 07:26:49 +0100

    Initialize PI-6743-a-user-can-refund-an-order

---
+++ frontend/src/Shop/components/SignIn.tsx
@@ -0,0 +1,30 @@
+import { Dialog, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material';
+
+/* DEVELOPER NOTE:
+* this card displays the Sign-In alert when a user is not signed in
+*/
+
+interface Props {
+  onSignIn: () => void,
+  onModalClose: () => void,
+  showModal: boolean,
+}
+
+export default function SignIn(props: Props) {
+  return (
+    <Dialog
+    open={props.showModal}
+    onClose={props.onModalClose}
+  >
+    <DialogContent>
+      <DialogContentText>
+          You need to sign in first.
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={props.onSignIn}>Sign In</Button>
+      <Button onClick={props.onModalClose}>Close</Button>
+    </DialogActions>
+  </Dialog>
+  )
+}
---
+++ frontend/src/Shop/index.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage  from "./pages"
+import UserToAppPayments from "./pages/UserToApp";
+import AppToUserPayments from "./pages/AppToUser";
+import AuthProvider from "./components/Auth";
+
+export default function Shop() {
+
+  return (
+    <>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/> 
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/payments/usertoapp" element={<UserToAppPayments />} key="UserToApp" />
+          <Route path="/payments/apptouser" element={<AppToUserPayments />} key="AppToUser" />
+        </Routes>
+      </AuthProvider>
+    </>
+  );
+}
